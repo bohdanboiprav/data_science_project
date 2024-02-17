c08e0e18b957a756b3eb73422b51d480a43083bd
Catalin Bondari <catalin.bondari2@mail.dcu.ie>
2024-02-17 12:21:15 +0000

    Code for stocks (TSLA Amazon etc)

--- Telegram Bot/config.py
+++ Telegram Bot/config.py
@@ -1,11 +1,9 @@
 from pydantic import ConfigDict
 from pydantic_settings import BaseSettings
 
-
 class Settings(BaseSettings):
-    TELEGRAM_API_KEY: str
+    TELEGRAM_API_KEY: str = "6777180102:AAF1TyJw-PfVunfKiIlckiY6v911NV44mfQ"
 
     model_config = ConfigDict(extra='ignore', env_file=".env", env_file_encoding="utf-8")  # noqa
-
 
 settings = Settings()
